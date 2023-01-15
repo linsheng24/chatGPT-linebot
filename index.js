93949efe6ad1bb0e24fe7764a5f714ddabd870f7
Jackson <jackson@ya-ya.tw>
2023-01-15 18:47:59 +0800

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+const line = require("@line/bot-sdk");
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function getChatReply(text = "") {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: text,
+    max_tokens: 1000,
+  });
+  return { type: "text", text: completion.data.choices[0].text.trim() };
+}
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
+const app = express();
+
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+async function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  const echo = await getChatReply(event.message.text);
+  return client.replyMessage(event.replyToken, echo);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
