4eee5fb72594115ca79575d27a98f427a165661a
Jackson <jackson@ya-ya.tw>
2023-01-16 00:20:38 +0800

    edit params

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ async function getChatReply(text = "") {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: text,
-    max_tokens: 1000,
+    max_tokens: 300,
+    temperature: 0.9,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
   });
   return { type: "text", text: completion.data.choices[0].text.trim() };
 }
