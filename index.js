d89f1ac3fde89a2817f20ba5eb0293205774891c
Jackson <jackson@ya-ya.tw>
2023-03-14 04:25:11 +0800

    feature: complete gpt3.5 model usage

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const line = require("@line/bot-sdk");
 const express = require("express");
 const axios = require("axios");
+const Redis = require("ioredis");
 require("dotenv").config();
 
-async function getChatReply(text = "") {
+const { REDIS_URL } = process.env;
+const renderRedis = new Redis(REDIS_URL);
+
+async function getChatReply(logs = []) {
   let data = JSON.stringify({
     "model": "gpt-3.5-turbo",
     "messages": [
-    { role: "system", content: 'test' },
-    { role: "assistant", content: "Hi. How can I help you today?" },
-    { role: "user", content: 'test' }],  
+      ...logs,
+    ],  
   });
     
   var config = {
@@ -51,16 +54,35 @@ app.post("/callback", line.middleware(config), (req, res) => {
       res.status(500).end();
     });
 });
-
 async function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
-
-  const echo = await getChatReply(event.message.text);
-  return client.replyMessage(event.replyToken, echo);
+  const message = event.message.text;
+  const userId = event?.source?.userId;
+  let reply;
+  if (message === '小寶貝早安！') {
+    reply = { type: 'text', text: '早安～～' };
+  } else if (message === '喝下孟婆湯吧！') {
+    reply = { type: 'text', text: '咕嚕咕嚕～～ 好喝' };
+    await renderRedis.del(`log_${userId}`)
+  } else {
+    try {
+      const userLog = { role: "user", content: message };
+      const userLogs = await renderRedis.get(`log_${userId}`);
+      let newLogs = !userLogs ? [userLog] : [...(JSON.parse(userLogs)).slice(-6), userLog];
+      await renderRedis.set(`log_${userId}`, JSON.stringify(newLogs));
+      reply = await getChatReply(newLogs);
+      await renderRedis.set(`log_${userId}`, JSON.stringify([
+        ...newLogs,
+        { role: "assistant", content: reply.text }
+      ]));
+    } catch (e) {
+      reply = { type: 'text', text: '我是小寶貝，我吃好飽，我想睡覺～～～zzZ' };
+    }
+  }
+  return client.replyMessage(event.replyToken, reply);
 }
-getChatReply('你好');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
